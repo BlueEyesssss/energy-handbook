1dbe37b406bf64cc2ae4bf6c87ab889cc8a1e83d
Pham Huynh Phuong Kha <vuamessi9@gmail.com>
2024-04-03 16:12:50 +0700

    first commit

---
+++ src/main/java/com/khaphp/energyhandbook/EnergyHandbookApplication.java
@@ -0,0 +1,13 @@
+package com.khaphp.energyhandbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyHandbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyHandbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaphp/energyhandbook/EnergyHandbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaphp.energyhandbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyHandbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
